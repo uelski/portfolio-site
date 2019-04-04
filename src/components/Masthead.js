ae387ed50a0d5640b14c48ba7a8065804810be79
Sam Vredenburgh <sam.vburgh@gmail.com>
2019-04-03 22:32:48 -0500

    removed masthead video and updated with svg background

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -4,9 +4,15 @@ class Masthead extends Component {
     render() {
         return (
             <div className="masthead-container">
-                <video autoPlay loop muted preload="true">
-                    <source src="/masthead_video.mp4" type="video/mp4" />
-                </video>
+                <div className="masthead-content-container">
+                    <h1>Sam Vredenburgh</h1>
+                    <ul>
+                        <li>Web Developer</li>
+                        <li>Synthesizer Enthusiast</li>
+                        <li>Skier</li>
+                    </ul>
+                    
+                </div>
             </div>
         )
     }
