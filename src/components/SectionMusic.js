3daa12b80593677d1201469f8e54eaacda873a47
Sam Vredenburgh <sam.vburgh@gmail.com>
2019-05-04 13:38:18 -0500

    starting painting carousel

--- src/components/SectionMusic.js
+++ src/components/SectionMusic.js
@@ -60,12 +60,6 @@ class Music extends Component {
                         <iframe width="100%" height="42" src="https://bandcamp.com/EmbeddedPlayer/album=1919140128/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="http://masoncos.bandcamp.com/album/time-being">Time Being by Mason Cos</a></iframe>
                     </div>
                 </div>
-                <div className="music-list-container">
-                    <h3>Jams</h3>
-                    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/415156917&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-                    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/398002605&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-                    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/342738157&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-                </div>
             </div>
         )
     }
